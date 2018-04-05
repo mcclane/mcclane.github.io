ead93c52f0b57fe10c1ea24337d3dd02c123ebf6
McClane Howland <mcclane.howland@gmail.com>
2018-04-05 10:57:17 -0700

    shortcut

--- game/game.js
+++ game/game.js
@@ -4,7 +4,7 @@ function createScene() {
     var scene = new BABYLON.Scene(engine);
     var camera = new BABYLON.UniversalCamera('camera', new BABYLON.Vector3(0, 5, -10), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, false);
+    camera.attachControl(canvas, true);
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
     var sphere = new BABYLON.MeshBuilder.CreateSphere('sphere', {segments: 16, diameter: 2}, scene);
     sphere.position.y = 1;
